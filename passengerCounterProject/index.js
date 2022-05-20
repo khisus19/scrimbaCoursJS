82aa67222cf5f22ff44b17306a1c461739120a2b
Jesús Álvarez <khisus19@gmail.com>
2022-05-20 07:48:13 -0500

    Passenger count app project finished

--- passengerCounterProject/index.js
+++ passengerCounterProject/index.js
@@ -1,18 +1,20 @@
-let count = 0;
 let countEl = document.getElementById("count-el");
-
+let saveEl = document.getElementById("save-el");
+let count = 0;
 
 
 function increment() {
-    count = count + 1;
-    countEl.innerText = count;
+    count += 1;
+    countEl.textContent = count;
 }
 
 function save() {
-    console.log(count);
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    reset();
 }
-/* function reset() {
+
+function reset() {
     count = 0;
-    countEl.innerText = count;;
-    return count;
-} */
+    countEl.textContent = count;
+}
