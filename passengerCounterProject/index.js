b8c940cc013928cb73b14a0f30682db9ce48815e
Jesús Álvarez <khisus19@gmail.com>
2022-05-19 22:07:08 -0500

    Adding the save button

--- passengerCounterProject/index.js
+++ passengerCounterProject/index.js
@@ -1,1 +1,18 @@
-document.getElementById("count").innerText = 5
+let count = 0;
+let countEl = document.getElementById("count-el");
+
+
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+}
+
+function save() {
+    console.log(count);
+}
+/* function reset() {
+    count = 0;
+    countEl.innerText = count;;
+    return count;
+} */
