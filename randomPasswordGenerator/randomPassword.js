1bdfdb853edb0b7fb7483e92cc9ff65dc26dea84
Jesús Álvarez <khisus19@gmail.com>
2022-05-27 18:36:23 -0500

    Functions ready

--- randomPasswordGenerator/randomPassword.js
+++ randomPasswordGenerator/randomPassword.js
@@ -1,5 +1,32 @@
-let charArry = [];
-var s = '';
-for (var i=32; i<=127;i++) {
-    s += String.fromCharCode(i);
+let charArray = [];
+
+let password1El = document.getElementById("pwd-el1");
+let password2El = document.getElementById("pwd-el2");
+let password3El = document.getElementById("pwd-el3");
+let password4El = document.getElementById("pwd-el4");
+
+for (var i = 32; i <= 127; i++) {
+    charArray.push(String.fromCharCode(i));
+}
+
+function randomChar() {
+    let randomIndex = Math.floor(Math.random() * 95);
+    return charArray[randomIndex];
+}
+
+function passwordizator(num){
+    let password1 = "";
+    let password2 = "";
+    let password3 = "";
+    let password4 = "";
+    for(let i = 0; i < num; i++){
+       password1 += randomChar();
+       password2 += randomChar();
+       password3 += randomChar();
+       password4 += randomChar();
+    }
+    password1El.textContent = password1;
+    password2El.textContent = password2;
+    password3El.textContent = password3;
+    password4El.textContent = password4;
 }
