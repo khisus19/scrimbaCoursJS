e3b7deb6373d0c8758c19219710431bf1e7c80c6
Jesús Álvarez <khisus19@gmail.com>
2022-05-29 08:56:11 -0500

    Copy function ready with responsive style

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# scrimbaCourseJS
+# ScrimbaCourseJS
 Projects I made following Scrimba's Javascript Free Course
+
+https://khisus19.github.io/scrimbaCourseJS/
--- randomPasswordGenerator/randomPassword.js
+++ randomPasswordGenerator/randomPassword.js
@@ -5,18 +5,29 @@ let password1El = document.getElementById("pwd-el1");
 let password2El = document.getElementById("pwd-el2");
 let password3El = document.getElementById("pwd-el3");
 let password4El = document.getElementById("pwd-el4");
+let passContainerCollection = document.getElementsByClassName("password-cont");
+let passContainerArray = [...passContainerCollection];
 
-for (var i = 33; i <= 127; i++) {
+for (var i = 33; i <= 126; i++) {
     charArray.push(String.fromCharCode(i));
 }
 
 function randomChar() {
-    let randomIndex = Math.floor(Math.random() * 95);
+    let randomIndex = Math.floor(Math.random() * 94);
     return charArray[randomIndex];
 }
 
 function passwordizator(){
     let charLength = inputEl.value;
+    if (charLength > 12){
+        for (let i = 0; i < passContainerArray.length; i++){
+            passContainerArray[i].style.width = "280px";
+        }
+    } else {
+        for (let i = 0; i < passContainerArray.length; i++){
+            passContainerArray[i].style.width = "220px";
+        } 
+    }
     password1 = "";
     password2 = "";
     password3 = "";
@@ -33,18 +44,10 @@ function passwordizator(){
     password4El.textContent = password4;
 }
 
-function copyClipboard(pass) {
-    // /* Get the text field */
-    // //var copyText = document.getElementById("pwd-el1");
-  
-    // /* Select the text field */
-    // copyText.select();
-    // copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  
+function copyClipboard(pass) {  
     //  /* Copy the text inside the text field */
     navigator.clipboard.writeText(pass);
   
     // /* Alert the copied text */
     console.log("Copied the text: " + pass);
-    
-  } 
+}
